d0b3d2e40f3874a485a2bbdab3b35445d3d48cb7
Jake <jakemny@gmail.com>
2015-05-11 19:35:48 -0400

    working on api

--- app/controllers.php
+++ app/controllers.php
@@ -19,7 +19,7 @@ function diamonds() {
 	$db = _get_db(); 
 	$sql = "SELECT * FROM `item` WHERE `actual stones` = 1 AND CHAR_LENGTH(`color grade`) = 1";
 
-	$sql .= isset($_GET["shape"]) ? " AND `Shape` = {$_GET['shape']}" : ""; 
+	$sql .= isset($_GET["shape"]) ? " AND `Shape` = '{$_GET['shape']}'" : ""; 
 
 	$query = $db->query($sql);
 	$result = $query->fetchAll(PDO::FETCH_ASSOC);
